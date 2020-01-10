3173017c6aae310cb9ba02f94e7966f9119030e4
Emilio Araos <ea3dev@gmail.com>
2020-01-10 13:11:38 -0700

    dictionary indexing

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+my_dict = {'key1':'value1', 'key2':'value2'}
+print(my_dict)
+print(my_dict['key1'])
+prices_lookup = {'apple': 3.50, 'oranges':1.99,'chocolate':0.50, 'gum': 1.0}
+print(prices_lookup['oranges'])
+other_dictionary = {'k1': 123, 'k2': [0,1,2], 'k3':{'insideKey':100}}
+print(other_dictionary)
+print(other_dictionary['k3'])
+print(other_dictionary['k3']['insideKey'])
+
+
